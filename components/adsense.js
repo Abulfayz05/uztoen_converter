47bc5a4198ef04396e52461f57e6a237d9f51af1
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-10-12 19:29:00 +0500

    adsense added

---
+++ components/adsense.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Script from 'next/script'
+export default function adsense() {
+  return (
+    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2034124662077944"
+     crossorigin="anonymous"></Script>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import Gtag from "../components/gtag";
+import Adsense from "../components/adsense";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Adsense />
       <Gtag />
       <Component {...pageProps} />
     </>
